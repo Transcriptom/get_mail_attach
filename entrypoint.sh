abb5ec9d15e87cb1b0deb99ee4868fd65c012ff5
seqyuan <ahworld07@gmail.com>
2021-01-11 22:51:24 +0800

    a

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -l
-/get_mail_attach -c $1 -u $2 -p $3 -d $4 && echo `pwd` && ls -l && ls -l /tmp && sh /tmp/output_context.sh
+/get_mail_attach -c $1 -u $2 -p $3 -d $4 && echo `pwd` && ls -l && ls -l /tmp && sh ./output_context.sh
