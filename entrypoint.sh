b88d7e2af3d79224706bd94e7c955d137a8000db
seqyuan <ahworld07@gmail.com>
2021-01-12 00:45:19 +0800

    v1.0

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,7 @@
 #!/bin/sh -l
-/get_mail_attach -c $1 -u $2 -p $3 -d $4 && echo `pwd` && ls -l && ls -l /tmp && sh ./output_context.sh
+/get_mail_attach -c $1 -u $2 -p $3 -d $4 
+if [ -f "./output_context.sh" ];then
+	sh ./output_context.sh
+else
+	echo "::set-output name=need_next_step::FALSE"
+fi
